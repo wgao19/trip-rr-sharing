8a961da1ccaa5505eefc04ce2846ad91d53e6699
Wei Gao <gaow@seagroup.com>
2018-08-01 19:07:06 +0800

    Prepare slides for trip sharing

--- presentation/index.js
+++ presentation/index.js
@@ -1,17 +1,14 @@
-// Import React
 import React from "react";
-
-// Import Spectacle Core tags
 import {
   BlockQuote,
   Cite,
   Deck,
   Heading,
-  ListItem,
-  List,
   Quote,
   Slide,
-  Text
+  Text,
+  Notes,
+  Typeface
 } from "spectacle";
 
 // Import theme
@@ -20,47 +17,78 @@ import createTheme from "spectacle/lib/themes/default";
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "#C1638E",
+    highlight: "#F78679",
+    dark: "#371749",
+    bg: "white"
+  },
+  {
+    primary: {
+      name: "Kirang Haerang",
+      googleFont: true
+    },
+    secondary: "Montserrat"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={500}
+        theme={theme}
+      >
+        <Slide>
+          <Heading size={1} fit caps lineHeight={1} textColor="dark">
+            Trip RR
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="white" size={1} fit>
+            Trip to React Rally, Aug 2018
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide
+          bgImage="https://cdn.dribbble.com/users/870476/screenshots/4363322/slc1.png"
+          bgSize="contain"
+          bgRepeat="no-repeat"
+          bgColor="dark"
+          key="slc-1"
+        >
+          <Heading size={1} fit caps lineHeight={1} textColor="dark">
+            Salt Lake City
+          </Heading>
+          <Notes>
+            <Text textFont="secondary">
+              <h4>Slide notes</h4>
+            </Text>
+            <ol>
+              <li>The Leonardo</li>
+              <li>The Great Salt Lake</li>
+            </ol>
+          </Notes>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+        <Slide>
+          <Heading size={1} fit caps lineHeight={1} textColor="dark">
+            Workshop
+          </Heading>
+          <Text textFont="secondary">Advanced React Component</Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+        <Slide bgColor="dark">
+          <Heading size={1} fit caps lineHeight={1} textColor="highlight">
+            React Rally
+          </Heading>
+          <Text textFont="secondary" textColor="primary">
+            The Real 💩
+          </Text>
+        </Slide>
+        <Slide>
+          <Heading size={1} fit caps lineHeight={1} textColor="dark">
+            Climbing
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="dark" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
